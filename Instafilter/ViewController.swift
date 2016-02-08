656f5061782f707ed5d97daf15c22a5b3be12ade
Ken Wilcox <wilcoxk@gmail.com>
2016-02-07 21:56:12 -0700

    Added a simple Sepia filter

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -12,10 +12,17 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var imageView: UIImageView!
   @IBOutlet weak var intensity: UISlider!
+  var currentImage: UIImage!
+  var context: CIContext!
+  var currentFilter: CIFilter!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    title = "Instafilter"
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "importPicture")
+    context = CIContext(options: nil)
+    currentFilter = CIFilter(name: "CISepiaTone")
   }
   
   override func didReceiveMemoryWarning() {
@@ -32,7 +39,48 @@ class ViewController: UIViewController {
   }
   
   @IBAction func intensityChanged(sender: AnyObject) {
+    applyProcessing()
+  }
+  
+  func importPicture() {
+    let picker = UIImagePickerController()
+    picker.allowsEditing = true
+    picker.delegate = self
+    presentViewController(picker, animated: true, completion: nil)
+  }
+  
+  func applyProcessing() {
+    currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
     
+    let cgimg = context.createCGImage(currentFilter.outputImage!, fromRect: currentFilter.outputImage!.extent)
+    let processedImage = UIImage(CGImage: cgimg)
+    
+    imageView.image = processedImage
   }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    var newImage: UIImage
+    
+    if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+      newImage = possibleImage
+    } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+      newImage = possibleImage
+    } else {
+      return
+    }
+    
+    dismissViewControllerAnimated(true, completion: nil)
+    
+    currentImage = newImage
+    let beginImage = CIImage(image: currentImage)
+    currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+    
+    applyProcessing()
+  }
+  
+  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+}
